42471b26857a28b7b1bf973d3fe15cdcc1aa73ff
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-03-17 20:38:48 +0700

    setup redux ke aplikasi

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
+import thunk from 'redux-thunk';
 import App from './app/App';
+import UIReducer from './app/store/reducers/ui';
+
+const rootReducer = combineReducers({
+  ui: UIReducer,
+});
+
+/* eslint-disable */
+const logger = store => {
+  return next => {
+    return action => {
+      const result = next(action);
+      return result;
+    };
+  };
+};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(logger, thunk))
+);
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  </>,
   document.querySelector('#root')
 );
 
