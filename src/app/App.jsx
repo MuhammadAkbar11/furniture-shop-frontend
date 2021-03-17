2536307e5e76973f047a1b32c4199d27670bd1c1
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-03-17 21:29:14 +0700

    mengatur route bersarang

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
 
 import Home from '@app/containers/Home';
+import AppAdmin from './admin/AppAdmin';
 
+/* eslint-disable */
 const App = () => {
-  /* eslint-disable */
+  const match = useRouteMatch();
 
   return (
     <>
       <Switch>
-        <Route path='/' exact component={Home} />
+        <Route exact path={match.path}>
+          <Home />
+        </Route>
+        <Route path={`${match.path}admin`}>
+          <AppAdmin />
+        </Route>
       </Switch>
     </>
   );
--- src/app/containers/Home.jsx
+++ src/app/containers/Home.jsx
@@ -1,20 +1,9 @@
 import React from 'react';
-import styles from '../../styles/scss/app.module.scss';
-// import Title from '@components/Title';
-// import SubTitle from '@components/SubTitle';
 
 const Home = () => {
   return (
     <>
-      <div className={styles.app}>
-        <h1 className={styles.appTitle}>
-          Webpack, React and Babel Boilerplate
-        </h1>
-        <h3 className={styles.subtitle}>
-          {/* eslint-disable */}
-          Support Css Modules and Scss
-        </h3>
-      </div>
+      <h1>Furniture</h1>
     </>
   );
 };
