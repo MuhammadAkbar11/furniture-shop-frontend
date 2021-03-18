1390a3dbacf4b109e4bb7f87a619d3d2ef171b4a
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-03-18 22:27:20 +0700

    membuat kostum tema dan membuat layout template

---
+++ src/app/components/Layouts/Layout.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+
+import {
+  // unstable_createMuiStrictModeTheme as createMuiTheme
+  createMuiTheme,
+  ThemeProvider as MuiThemeProvider,
+  // darken,
+} from '@material-ui/core/styles';
+import { ThemeProvider } from 'styled-components';
+import PropTypes from 'prop-types';
+import TopMenu from './TopMenu/TopMenu';
+import { Wrapper } from './LayoutStyled';
+
+const defaultProps = {
+  children: null,
+};
+
+const proptypes = {
+  children: PropTypes.node,
+};
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#33685f',
+      dark: '#274742',
+      light: '#41796f',
+    },
+    secondary: {
+      main: '#f2f2f2',
+    },
+    white: 'f2f2f2',
+    slate: '#B9D8D3',
+    light: '#f9fafb',
+    dark: '#252525',
+    gray: {
+      light: '#9ca3af',
+      main: '#6b7280',
+      dark: '#374151',
+    },
+    success: {
+      main: '#10b981',
+    },
+    warning: {
+      main: '#f59e0b',
+    },
+    danger: {
+      main: '#ef4444',
+    },
+  },
+});
+
+const Layout = props => {
+  const { children } = props;
+  return (
+    <MuiThemeProvider theme={theme}>
+      <ThemeProvider theme={theme}>
+        <Wrapper>
+          <TopMenu />
+          {children}
+        </Wrapper>
+      </ThemeProvider>
+    </MuiThemeProvider>
+  );
+};
+
+Layout.defaultProps = defaultProps;
+
+Layout.propTypes = proptypes;
+
+export default Layout;
---
+++ src/app/components/Layouts/LayoutStyled.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable */
+
+import { Box } from '@material-ui/core';
+import styled from 'styled-components';
+
+export const Wrapper = styled(Box)`
+  margin: 0;
+  min-height: 100vh;
+  width: 100%;
+`;
