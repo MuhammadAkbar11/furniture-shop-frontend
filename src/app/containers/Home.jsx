bb668535a8f7f19608e7abd3a700257293ca9608
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-03-20 00:55:35 +0700

    import RecentProduct component

--- src/app/containers/Home.jsx
+++ src/app/containers/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from '../components/home/banner/Banner';
 import TrendCategories from '../components/home/catagories/TrendCategories';
+import RecentProduct from '../components/home/recentProducts/RecentProduct';
 import Layout from '../components/Layouts/Layout';
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
     <Layout>
       <Banner />
       <TrendCategories />
+      <RecentProduct />
     </Layout>
   );
 };
