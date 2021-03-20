0cbbb61615c84883efdd192232addbb1d87e0131
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-03-20 23:03:12 +0700

    ,engimport linksOverlay komponen

--- src/app/containers/Home.jsx
+++ src/app/containers/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from '../components/home/banner/Banner';
 import TrendCategories from '../components/home/catagories/TrendCategories';
+import LinksOverlay from '../components/home/LinksOverlay/LinksOvelay';
 import RecentProduct from '../components/home/recentProducts/RecentProduct';
 import Layout from '../components/Layouts/Layout';
 
@@ -10,6 +11,7 @@ const Home = () => {
       <Banner />
       <TrendCategories />
       <RecentProduct />
+      <LinksOverlay />
     </Layout>
   );
 };
