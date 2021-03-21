215e512c995cac156ab27c5e6ec158b9893154d3
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-03-21 22:11:42 +0700

    update ikon2

--- src/app/components/UI/Icons/SearchIcon.jsx
+++ src/app/components/UI/Icons/SearchIcon.jsx
@@ -1,32 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Icon = styled.svg`
+  height: 100%;
+  width: 100%;
+
+  .cls-1 {
+    fill: none;
+    stroke: currentColor;
+    stroke-linecap: round;
+    stroke-linejoin: bevel;
+    stroke-width: 1.5px;
+  }
+`;
 
 const SearchIcon = () => {
   return (
-    <svg
-      width='24px'
-      height='24px'
-      viewBox='0 0 24 24'
-      fill='none'
-      xmlns='http://www.w3.org/2000/svg'
-    >
-      <circle
-        cx='11.7666'
-        cy='11.7666'
-        r='8.98856'
-        stroke='currentColor'
-        stroke-width='1.5'
-        stroke-linecap='round'
-        stroke-linejoin='round'
-      />
-      <path
-        opacity='0.4'
-        d='M18.0183 18.4851L21.5423 22'
-        stroke='currentColor'
-        stroke-width='1.5'
-        stroke-linecap='round'
-        stroke-linejoin='round'
-      />
-    </svg>
+    <Icon viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+      <g id='ic-actions-search'>
+        <circle className='cls-1' cx='13.79' cy='10.15' r='7' />
+        <line className='cls-1' x1='8.91' y1='15.17' x2='4.03' y2='20.05' />
+      </g>
+    </Icon>
   );
 };
 
