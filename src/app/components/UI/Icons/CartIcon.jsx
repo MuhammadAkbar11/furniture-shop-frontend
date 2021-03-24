753b1e1d4ee111e4f4461b3844cff4a956e177ed
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-03-24 12:34:02 +0700

    fix color pada ikon keranjang

--- src/app/components/UI/Icons/CartIcon.jsx
+++ src/app/components/UI/Icons/CartIcon.jsx
@@ -3,8 +3,11 @@ import React from 'react';
 const CartIcon = () => {
   return (
     <svg
-      width='100%'
-      height='100%'
+      style={{
+        display: 'block',
+        height: '100%',
+        width: '100%',
+      }}
       viewBox='0 0 24 24'
       xmlns='http://www.w3.org/2000/svg'
     >
@@ -22,7 +25,10 @@ const CartIcon = () => {
             points='1.68 2.77 5.18 2.77 8.34 16.48 21 16.48 22.32 6.72 6.09 6.72'
           />
           <rect
-            className='cls-1'
+            style={{
+              fill: 'none',
+              stroke: 'currentcolor',
+            }}
             x='9.3'
             y='18.62'
             width='2.61'
@@ -30,7 +36,10 @@ const CartIcon = () => {
             rx='1.3'
           />
           <rect
-            className='cls-1'
+            style={{
+              fill: 'none',
+              stroke: 'currentcolor',
+            }}
             x='9.3'
             y='18.62'
             width='2.61'
@@ -38,7 +47,10 @@ const CartIcon = () => {
             rx='1.3'
           />
           <rect
-            className='cls-1'
+            style={{
+              fill: 'none',
+              stroke: 'currentcolor',
+            }}
             x='16.16'
             y='18.62'
             width='2.61'
