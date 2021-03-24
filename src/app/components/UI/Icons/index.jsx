087e3263bf27783883440b34b37e8b3dccf0b707
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-03-24 12:47:50 +0700

    Membuat komponen Ikon Quick View

--- src/app/components/UI/Icons/index.jsx
+++ src/app/components/UI/Icons/index.jsx
@@ -2,3 +2,4 @@
 export { default as CartIcon } from './CartIcon';
 export { default as UserIcon } from './ProfileIcon';
 export { default as SearchIcon } from './SearchIcon';
+export { default as QuickViewIcon } from './QuickViewIcon';
